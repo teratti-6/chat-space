cdc5e7fd2b0c99187f22f5108bc33f8734c2af0a
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2019-11-22 00:51:20 +0900

    グループ編集ページへのリンクまで実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+  resources :groups, only: [:new, :create, :edit, :update, :set_group] do
+    resources :messages, only: [:index, :create]
   end
 end
