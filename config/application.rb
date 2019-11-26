b6d88ebb3e1e6947a76f21ad3f93df8c2ecc7574
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2019-11-26 19:46:09 +0900

    自動deploy実装

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
     end
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
