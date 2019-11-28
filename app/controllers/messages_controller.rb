457aa0ccb8208ba0048141ff145405b76f900fa7
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2019-11-28 20:49:11 +0900

    画像の保存

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
+    if @message.save!
       respond_to do |format|
         format.html { redirect_to group_messages_path, notice: "メッセージを送信しました" }
         format.json
